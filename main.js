803edcfd55c1fe61476101d3cd583ae1aff5ce11
Mariacecilia1999 <mariaceciliaconti1999@gmail.com>
2023-12-12 17:21:37 -0300

    Adding local storage

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const $ = (selector) => document.querySelector(selector)
 const id = () => self.crypto.randomUUID()
+const get = (key) => JSON.parse(localStorage.getItem(key))
+const set = (key, arr) => (localStorage.setItem(key, JSON.stringify(arr)))
 
 const users = [{
    name: 'Ana', 
    id: id()
 }]
+
 
 
 const  btnAddUser = (e) =>{
@@ -13,10 +16,19 @@ const  btnAddUser = (e) =>{
       name: $('#inputName').value,
       id: id()
 })
-$('#inputName').value = ''
-console.log(users)
+   $('#inputName').value = ''
+   set('users', users)
 }
 
 
+const  initializer = () =>{
+   if(!get('users')){
+      set('users', users)
+   }else{
+      get('users')
+   }
+   $('#btnAddUser').addEventListener('click', btnAddUser)
+}
 
-$('#btnAddUser').addEventListener('click', btnAddUser)
+window.addEventListener('load', initializer)
+
