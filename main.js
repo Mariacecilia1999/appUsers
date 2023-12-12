ca9db227a09e9cdfdcf6e0191bd75ba7ba79bac6
Mariacecilia1999 <mariaceciliaconti1999@gmail.com>
2023-12-12 16:51:54 -0300

    Adding array with user objects

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-
 const $ = (selector) => document.querySelector(selector)
+const id = () => self.crypto.randomUUID()
+
+const users = [{
+   name: 'Ana', 
+   id: id()
+}]
 
 
 const  btnAddUser = (e) =>{
    e.preventDefault()
-   console.log($('#inputName').value)
+   users.push({
+      name: $('#inputName').value,
+      id: id()
+})
+$('#inputName').value = ''
+console.log(users)
 }
+
+
 
 $('#btnAddUser').addEventListener('click', btnAddUser)
